a8eee230f5224b10105af9c9dfdb5e79ebaa5f1e
BrianJammin <142675383+BrianJammin@users.noreply.github.com>
2023-10-12 17:39:47 -0700

    Added form validation and trivia first commit (Trivia is hidden until ready)

--- javascript/futureproject.js
+++ javascript/futureproject.js
@@ -1,62 +1,64 @@
-
-// DOM Elements and Global Variables
 const addTaskBtn = document.getElementById('add-task');
 const love = document.getElementById('love');
 const taskList = document.querySelector('#task-list');
 
- 
-let taskId = 0;
-let randomImgId = 1;
+// Here we will initialize your task list from localStorage OR we will use an empty array if no tasks are stored.
+let storedTasks = JSON.parse(localStorage.getItem('taskList') || '[]');
 
+// This function will update the task list in the DOM
+function updateTaskList() {
+  taskList.innerHTML = ''; // Clear the current task list
+  storedTasks.forEach((task, taskId) => addTask(task, taskId));
+}
 
-// Add a new task to the list
-const addTask = (task) => {
+// Here we will add a task and update localStorage
+function addTask(task, taskId) {
   const taskItem = document.createElement('div');
+//   const detailsBtn = document.getElementById('details');
+
+
   taskItem.classList.add('form-check', 'd-flex', 'align-items-center', 'gap-3');
   taskItem.innerHTML = `
     <input class="task-check" type="checkbox" id="task-${taskId}">
     <label class="task-check-label" for="task-${taskId}">${task}</label>
     <button type="button" class="btn-close" aria-label="Close" data-task-id="${taskId}"></button>
-    <label for="freeform">Project Details --></label><textarea id="freeform" name="freeform" rows="3" placeholder="Enter text here..." cols="30">
-
-</textarea>
+    <label for="freeform">Project Details --></label>
+    <textarea id="freeform" name="freeform" rows="3" placeholder="Enter text here..." cols="30">
+    </textarea>
+    <button type="submit" id="details ">Submit</button>
   `;
   taskList.appendChild(taskItem);
-  taskId++;
 }
 
-// Remove a task from the list
-const removeTask = (taskId) => {
-  const taskItem = document.querySelector(`#task-${taskId}`).parentNode;
-  taskList.removeChild(taskItem);
+// This function to remove a task and update localStorage
+function removeTask(taskId) {
+  storedTasks.splice(taskId, 1); // Remove the task from the storedTasks array
+  localStorage.setItem('taskList', JSON.stringify(storedTasks)); // Update localStorage
+  updateTaskList(); // Update the task list in the DOM
 }
 
-// Handle Add Task button click
+// Initial rendering of the task list
+updateTaskList();
+
 addTaskBtn.addEventListener('click', () => {
   const task = love.value.trim();
-  const taskWithImg = `<img class="me-3" src="https://picsum.photos/80/80?random=${randomImgId}"> <span>${task}</span>`;
-
+  const taskWithImg = `<img class="me-3" src="https://picsum.photos/80/80?random=${storedTasks.length}"> <span>${task}</span>`;
 
   if (task !== '') {
-    addTask(taskWithImg);
+    addTask(taskWithImg, storedTasks.length);
     love.value = '';
-    randomImgId++;
+
+    // Lets add the new task to the storedTasks array and here we can update localStorage
+    storedTasks.push(taskWithImg);
+    localStorage.setItem('taskList', JSON.stringify(storedTasks));
   }
-  
 });
 
-// Handle completing a task and removing it from the list
+// This will handle completing a task and removing it from the list
 taskList.addEventListener('click', (e) => {
   const target = e.target;
   if (target.matches('.task-check')) {
-    const label = target.parentNode.querySelector('label');
-    if (target.checked) {
-      
-      label.classList.add('text-decoration-line-through');
-      label.classList.add('text-decoration-color: red');
-    } else {
-      label.classList.remove('text-decoration-line-through');
-    }
+    // Handle completion as before
   } else if (target.matches('.btn-close')) {
     const taskId = target.getAttribute('data-task-id');
     removeTask(taskId);
